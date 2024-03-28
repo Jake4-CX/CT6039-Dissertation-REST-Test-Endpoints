323c7e8117b1dc0e31ec33779c2669fc788ea496
= <jake44264@hotmail.co.uk>
2024-03-28 16:17:53 +0000

    feat: auto deploy on droplet

---
+++ Go Lang/deployments/initialization-script.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Stop execution if any command fails
+set -e
+
+# Update and install necessary packages
+echo "Updating package list..."
+sudo apt-get update -y
+
+echo "Installing necessary digitalocean packages..."
+wget -qO- https://repos-droplet.digitalocean.com/install.sh | sudo bash
+
+# Check if Docker is installed, install if it's not
+if ! command -v docker &> /dev/null
+then
+    echo "Docker is not installed. Installing Docker..."
+    sudo apt-get install -y docker.io
+else
+    echo "Docker is already installed."
+fi
+
+# Ensure Docker service is running
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Pull the Docker image
+echo "Pulling the Docker image..."
+sudo docker pull jake4/loadtest-test-api:latest
+
+echo "Running the Docker container..."
+sudo docker run -d -e REST_PORT=8080 -p 8080:8080 jake4/loadtest-test-api:latest
+
+echo "Container deployed successfully!"
